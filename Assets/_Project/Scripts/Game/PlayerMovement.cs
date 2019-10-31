631ca7ad3618a6f949c36b19c910a952f4551476
Kuvasz <kuvasz013@gmail.com>
2019-10-31 16:34:33 +0100

    FEAT: Player control experimentation

---
+++ Assets/_Project/Scripts/Game/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public GameObject player;
+    public Camera mainCamera;
+    public float speed;
+    private float thrust;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Object Start");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveVertical = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        float moveHorizontal = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+ 
+        player.transform.Translate(moveHorizontal, moveVertical, 0);
+        mainCamera.transform.Translate(moveHorizontal, moveVertical, 0);
+    }
+}
